9b8c130f1e2095cec217cab0b89c1eb3fd9a4e4a
Jake Russell <jake.russell@valtech.com>
2024-01-12 17:57:43 +0000

    Updated styling for input form

--- src/components/NewLocationForm.tsx
+++ src/components/NewLocationForm.tsx
@@ -5,6 +5,8 @@ import Input from "./UI/Input.tsx";
 import Button from "./UI/Button.tsx";
 import { emojiCodePoints } from "../utils/emojis.ts";
 
+import "./NewLocationForm.scss";
+
 const NewLocationForm = () => {
   const {
     value: countryValue,
@@ -22,6 +24,30 @@ const NewLocationForm = () => {
     handleReset: resetCity,
   } = useInput("", (value) => isNotEmpty(value));
 
+  const {
+    value: flightPriceValue,
+    handleInputChange: handleFlightPriceChange,
+    handleInputBlur: handleFlightPriceBlur,
+    hasError: flightPriceHasError,
+    handleReset: resetFlightPrice,
+  } = useInput("", (value) => isNotEmpty(value));
+
+  const {
+    value: hotelPriceValue,
+    handleInputChange: handleHotelPriceChange,
+    handleInputBlur: handleHotelPriceBlur,
+    hasError: hotelPriceHasError,
+    handleReset: resetHotelPrice,
+  } = useInput("", (value) => isNotEmpty(value));
+
+  const {
+    value: flightDurationValue,
+    handleInputChange: handleFlightDurationChange,
+    handleInputBlur: handleFlightDurationBlur,
+    hasError: flightDurationHasError,
+    handleReset: resetFlightDuration,
+  } = useInput("", (value) => isNotEmpty(value));
+
   const isFormValid = isNotEmpty(countryValue) && isNotEmpty(cityValue);
 
   const handleSubmit = (event) => {
@@ -31,6 +57,9 @@ const NewLocationForm = () => {
 
     resetCountry();
     resetCity();
+    resetFlightPrice();
+    resetHotelPrice();
+    resetFlightDuration();
   };
 
   return (
@@ -50,6 +79,42 @@ const NewLocationForm = () => {
         onChange={handleCityChange}
         value={cityValue}
         error={cityHasError && "Please enter a valid city."}
+      />
+      <div className="new-location-form__price-fields">
+        <Input
+          label="Flight Price"
+          id="flight_price"
+          type="number"
+          min="0"
+          onBlur={handleFlightPriceBlur}
+          onChange={handleFlightPriceChange}
+          value={flightPriceValue}
+          error={flightPriceHasError && "Please enter a valid flight price."}
+        />
+        <Input
+          label="Hotel Price"
+          id="hotel_price"
+          type="number"
+          min="0"
+          onBlur={handleHotelPriceBlur}
+          onChange={handleHotelPriceChange}
+          value={hotelPriceValue}
+          error={hotelPriceHasError && "Please enter a valid hotel price."}
+        />
+      </div>
+
+      <Input
+        label="Flight Duration (Minutes)"
+        id="flight_duration"
+        type="number"
+        min="0"
+        onBlur={handleFlightDurationBlur}
+        onChange={handleFlightDurationChange}
+        value={flightDurationValue}
+        error={
+          flightDurationHasError &&
+          "Please enter a valid flight duration in minutes."
+        }
       />
       <div className="button-container">
         <Button
--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -4,15 +4,17 @@ import "./Input.scss";
 const Input = ({ label, id, type = "text", error, ...props }) => {
   return (
     <div className="input">
-      <label htmlFor={id} className="input__label">{label}</label>
+      <label htmlFor={id} className="input__label">
+        {label}
+      </label>
       <input
         id={id}
         name={id}
         type={type}
-        placeholder={`Please enter your ${label.toLowerCase()}...`}
+        placeholder={`Please enter the ${label.toLowerCase()}...`}
         required
         {...props}
-        className={`input__field ${error && 'input__field--error'}`}
+        className={`input__field ${error && "input__field--error"}`}
       />
       {error && <p className="input__error">{error}</p>}
     </div>
