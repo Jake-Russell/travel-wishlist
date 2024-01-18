ac21a9c1662c636b603dd56525d93ab012ed22d9
Jake <jake@megster.net>
2024-01-18 09:41:45 +0000

    Publish to firebase on form submission

--- src/components/NewLocationForm.tsx
+++ src/components/NewLocationForm.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { useInput } from "../hooks/useInput.tsx";
-import { isNotEmpty } from "../utils/inputValidation.ts";
-import Input from "./UI/Input.tsx";
-import Button from "./UI/Button.tsx";
-import { emojiCodePoints } from "../utils/emojis.ts";
+import React from 'react';
+import { useInput } from '../hooks/useInput.tsx';
+import { isNotEmpty } from '../utils/inputValidation.ts';
+import Input from './UI/Input.tsx';
+import Button from './UI/Button.tsx';
+import { emojiCodePoints } from '../utils/emojis.ts';
 
-import "./NewLocationForm.scss";
+import firebase from 'firebase/app';
+import {
+  onSnapshot,
+  collection,
+  query,
+  orderBy,
+  addDoc,
+} from 'firebase/firestore';
+import { db } from '../firebase.js';
+
+import './NewLocationForm.scss';
 
 const NewLocationForm = () => {
   const {
@@ -14,7 +24,7 @@ const NewLocationForm = () => {
     handleInputBlur: handleCountryBlur,
     hasError: countryHasError,
     handleReset: resetCountry,
-  } = useInput("", (value) => isNotEmpty(value));
+  } = useInput('', (value) => isNotEmpty(value));
 
   const {
     value: cityValue,
@@ -22,7 +32,7 @@ const NewLocationForm = () => {
     handleInputBlur: handleCityBlur,
     hasError: cityHasError,
     handleReset: resetCity,
-  } = useInput("", (value) => isNotEmpty(value));
+  } = useInput('', (value) => isNotEmpty(value));
 
   const {
     value: flightPriceValue,
@@ -30,7 +40,7 @@ const NewLocationForm = () => {
     handleInputBlur: handleFlightPriceBlur,
     hasError: flightPriceHasError,
     handleReset: resetFlightPrice,
-  } = useInput("", (value) => isNotEmpty(value));
+  } = useInput('', (value) => isNotEmpty(value));
 
   const {
     value: hotelPriceValue,
@@ -38,7 +48,7 @@ const NewLocationForm = () => {
     handleInputBlur: handleHotelPriceBlur,
     hasError: hotelPriceHasError,
     handleReset: resetHotelPrice,
-  } = useInput("", (value) => isNotEmpty(value));
+  } = useInput('', (value) => isNotEmpty(value));
 
   const {
     value: flightDurationValue,
@@ -46,20 +56,37 @@ const NewLocationForm = () => {
     handleInputBlur: handleFlightDurationBlur,
     hasError: flightDurationHasError,
     handleReset: resetFlightDuration,
-  } = useInput("", (value) => isNotEmpty(value));
+  } = useInput('', (value) => isNotEmpty(value));
 
   const isFormValid = isNotEmpty(countryValue) && isNotEmpty(cityValue);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    alert(`Thank you for submitting the form!`);
+    // / Create a Firestore document reference
+    const collectionRef = collection(db, 'wishlist');
+    const locationData = {
+      country: countryValue,
+      city: cityValue,
+      flightPrice: parseFloat(flightPriceValue) || 0,
+      hotelPrice: parseFloat(hotelPriceValue) || 0,
+      flightDuration: parseInt(flightDurationValue) || 0,
+    };
 
-    resetCountry();
-    resetCity();
-    resetFlightPrice();
-    resetHotelPrice();
-    resetFlightDuration();
+    try {
+      // Add data to Firestore using addDoc
+      const docRef = await addDoc(collectionRef, locationData);
+      console.log('Location data added to Firestore with ID:', docRef.id);
+
+      // Reset form fields
+      resetCountry();
+      resetCity();
+      resetFlightPrice();
+      resetHotelPrice();
+      resetFlightDuration();
+    } catch (error) {
+      console.error('Error adding location to Firestore:', error);
+    }
   };
 
   return (
@@ -70,7 +97,7 @@ const NewLocationForm = () => {
         onBlur={handleCountryBlur}
         onChange={handleCountryChange}
         value={countryValue}
-        error={countryHasError && "Please enter a valid country."}
+        error={countryHasError && 'Please enter a valid country.'}
       />
       <Input
         label="City"
@@ -78,7 +105,7 @@ const NewLocationForm = () => {
         onBlur={handleCityBlur}
         onChange={handleCityChange}
         value={cityValue}
-        error={cityHasError && "Please enter a valid city."}
+        error={cityHasError && 'Please enter a valid city.'}
       />
       <div className="new-location-form__price-fields">
         <Input
@@ -89,7 +116,7 @@ const NewLocationForm = () => {
           onBlur={handleFlightPriceBlur}
           onChange={handleFlightPriceChange}
           value={flightPriceValue}
-          error={flightPriceHasError && "Please enter a valid flight price."}
+          error={flightPriceHasError && 'Please enter a valid flight price.'}
         />
         <Input
           label="Hotel Price"
@@ -99,7 +126,7 @@ const NewLocationForm = () => {
           onBlur={handleHotelPriceBlur}
           onChange={handleHotelPriceChange}
           value={hotelPriceValue}
-          error={hotelPriceHasError && "Please enter a valid hotel price."}
+          error={hotelPriceHasError && 'Please enter a valid hotel price.'}
         />
       </div>
 
@@ -113,7 +140,7 @@ const NewLocationForm = () => {
         value={flightDurationValue}
         error={
           flightDurationHasError &&
-          "Please enter a valid flight duration in minutes."
+          'Please enter a valid flight duration in minutes.'
         }
       />
       <div className="button-container">
